fd995beb3c46319c512351ce5c70122f6d5ebeda
lorenzo <Vous@exemple.com>
2021-04-24 14:01:01 +0200

    v1

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,27 +9,27 @@ class Main
         Scanner message = new Scanner(System.in);
         SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm");
        
-
         do {
-            
             System.out.println("Entrez une chaine de charactéres ou 'exit' pour mettre fin au programme : ");
             try
-            {    String chaine = message.nextLine() ; 
+            {    
+                String chaine = message.nextLine() ; 
                 if (chaine.equals("exit"))
                     System.exit(0);
-                Traitement.verification("nom");
-                //Date date = new Date();
-                //int calcul = Traitement.calcul(chaine) ; 
-               // Traitement.ajout(s.format(date), chaine , calcul );
-                //System.out.println(chaine + " contient "+ calcul +" characteres unique ");
+                if (!Traitement.verification(chaine)) 
+                {
+                    Date date = new Date();
+                    int calcul = Traitement.calcul(chaine) ; 
+                    Traitement.ajout(s.format(date), chaine , calcul );
+                    System.out.println(chaine + " contient "+ calcul +" characteres unique ");
+                }
             }
             catch(Exception e ) 
             {
                 System.out.println("Erreur .... ");
             }
-        } while (true);
 
-       
+        } while (true);
     }
 
 }
--- Traitement.java
+++ Traitement.java
@@ -11,50 +11,59 @@ class Traitement
     {
         try 
         {
-            FileWriter fichierCsv = new FileWriter(nomFichier,true); 
-            fichierCsv.append( date +";"+ nom +";"+ nombre + "\n") ;  
-            fichierCsv.close();
+            FileWriter fichier = new FileWriter(nomFichier,true); 
+            fichier.append( date +";"+ nom +";"+ nombre + "\n") ;  
+            fichier.close();
         } 
         catch (Exception e) 
         {
-            System.out.println("Erreur fichier ");
+            System.out.println("Erreur ecriture fichier ");
         }    
     }
 
-    public static void verification(String nom )
+    public static Boolean verification(String chaine ) 
     {
         String ligne ; 
         
         try
         {
-	        BufferedReader lecteurAvecBuffer = new BufferedReader(new FileReader(nomFichier));
+	        BufferedReader lectureFichier = new BufferedReader(new FileReader(nomFichier));
+            while ((ligne = lectureFichier.readLine()) != null)
+            {
+                String[] elementLigne = ligne.split(";", 3);
+  
+               if (chaine.equals(elementLigne[1]))
+               {
+                    System.out.println("calcul effectuer le "+ elementLigne[0] + " retournant le resultat de " + elementLigne[2] +" characteres unique dans la chaine ");
+                    lectureFichier.close();
+                    return true ; 
+               }
+            }
+            lectureFichier.close();
         }
         catch(Exception exc)
         {
-	        System.out.println("Erreur d'ouverture");
+	        System.out.println("Erreur d'ouverture fichier ");
         }
-        while ((ligne = lecteurAvecBuffer.readLine()) != null)
-        {
-            System.out.println(ligne);
-        }
-        lecteurAvecBuffer.close();
+        
+        return false ; 
         
     }
 
-    public static  int calcul(String data )
+    public static  int calcul(String chaine )
     {
         ArrayList<Character> liste = new ArrayList<>();
 
-        for (int i =0 ; i <data.length(); i++)
-            liste.add(data.charAt(i));
+        for (int i =0 ; i <chaine.length(); i++)
+            liste.add(chaine.charAt(i));
             int nombre = 0 ;
             for  (Character el1 : liste)
             {   
                 int i = 0 ;
                 for ( Character el2 : liste )
                 { 
-                if (el2.equals(el1))
-                    i++ ; 
+                    if (el2.equals(el1))
+                        i++ ; 
                 }
                if (i == 1 )
                     nombre ++ ; 
