2427287bda8fb0485c979835c0bd68f28f2ac037
lorenzo <Vous@exemple.com>
2021-04-24 16:26:44 +0200

    code commenter

--- Main.java
+++ Main.java
@@ -6,30 +6,32 @@ class Main
 {
     public static void main(String[] args)
     {
-        Scanner message = new Scanner(System.in);
-        SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Scanner message = new Scanner(System.in);   // CRÉATION OBJET DE TYPE SCANNER 
+        SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy HH:mm"); // CRÉATION OBJET "s"  DE TYPE FROMATDATE 
+        Traitement.cache(); // METHODE QUI CRÉE LE FICHIER CACHE  OU LE SUPPRIME SI SON POID ET TROP IMPORTANT 
        
-        do {
+        do {  // DO WHILE CAR ON VEUT LANCER LE CODE AU MOINS UNE FOIS 
             System.out.println("Entrez une chaine de charactéres ou 'exit' pour mettre fin au programme : ");
             try
             {    
                 String chaine = message.nextLine() ; 
-                if (chaine.equals("exit"))
+                if (chaine.equals("exit"))   // SI LA SAISIE VAUT "exit"  ON FERME PROPREMENT LE PROGRAMME 
                     System.exit(0);
-                if (!Traitement.verification(chaine)) 
+                if (!Traitement.verificationDepuisCache(chaine))  // SI LA SAISIE N'EST PAS DEJA DANS LE FICHIER  (SI LA METHODE RETURN FALSE)
                 {
-                    Date date = new Date();
-                    int calcul = Traitement.calcul(chaine) ; 
-                    Traitement.ajout(s.format(date), chaine , calcul );
-                    System.out.println(chaine + " contient "+ calcul +" characteres unique ");
+                    Date date = new Date();   // ON CRÉE UN OBJET "date" DE TYPE DATE POUR AVOIR LA DATE ACTUELLE 
+                    int calcul = Traitement.calcul(chaine) ;  // ON CACUL LE NOMBRE DE CHARACTÉRES UNIQUE DANS LA CHAINE 
+                    Traitement.ajoutCache(s.format(date), chaine , calcul ); // ON AJOUTE AU FICHIER LES NOUVELLE INFO (DATE , CHAINE , LE RÉSULTAT DU CALCUL)
+                    System.out.println(chaine + " contient " +  calcul + " characteres unique "); // ON AFFICHE A L'UTILISATEUR  LA CHAINE SAISIE ET SON NOMBRE DE CHARACTERES UNIQUE
                 }
             }
-            catch(Exception e ) 
+            catch(Exception e )   // CAPTURE DES EXCEPTION GENERAL (C'EST MAL )  EN CAS PROBLEME 
             {
                 System.out.println("Erreur .... ");
             }
 
-        } while (true);
+        } while (true); // TRUE  CAR ON VEUT UNE BOUCLE INFI JUSQU'A CE QUE L'UTILSATEUR TAPE "exit" 
+        
     }
 
 }
--- Traitement.java
+++ Traitement.java
@@ -2,41 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.io.File;
 
-class Traitement
+class Traitement  // CLASSE SANS CONSTRUCTEUR CAR CRÉATION OBJET NON UTILES 
 {
-    static private  String nomFichier = ".cache" ; 
+    static private  String nomFichier = ".cache" ; // STATIC CAR LIER A AUCUN OBJET  PRIVATE CAR NE DOIT PAS ETRE MODIFIER 
 
-    public static void  ajout(String date , String nom , int nombre  )
+    public static void  ajoutCache(String date , String nom , int nombre  )  // METHODE D'AJOUT AU FICHIER CACHE STATIC CAR NON LIER A UN OBJET 
     {
         try 
         {
-            FileWriter fichier = new FileWriter(nomFichier,true); 
-            fichier.append( date +";"+ nom +";"+ nombre + "\n") ;  
-            fichier.close();
+            FileWriter fichier = new FileWriter(nomFichier,true);     //  PARAMETTRE TRUE  CAR LE FICHIER EXISTE DEJA 
+            fichier.append( date +";"+ nom +";"+ nombre + "\n") ;     // AJOUT DES INFO AU FICHIER  \N  POUR RETOUR A LA LIGNE
+            fichier.close();                                         // FERMETURE DU FICHIER POUR L'ENVOIE ENTRE LE FICHIER LOGIQUE ET PHYSIQUE 
+                                                                    //  MULTIPLE OUVERTURE ET FERMETURE NON OPTI MAIS OBLIGATOIRE DU SYSTEM.EXIT      
         } 
-        catch (Exception e) 
+        catch (Exception e ) 
         {
-            System.out.println("Erreur ecriture fichier ");
+            System.out.println("Erreur d'ecriture fichier ");
         }    
     }
 
-    public static Boolean verification(String chaine ) 
+    public static Boolean verificationDepuisCache(String chaine )  // METHODE QUI VÉRIFIE SI LA CHAINE PASSER EST PRÉSENTE DANS LE FICHIER SI OUI RETOUR TRUE 
     {
         String ligne ; 
         
         try
         {
 	        BufferedReader lectureFichier = new BufferedReader(new FileReader(nomFichier));
-            while ((ligne = lectureFichier.readLine()) != null)
+            while ((ligne = lectureFichier.readLine()) != null)  // PARCOURS FICHIER LIGNE PAR LIGNE 
             {
-                String[] elementLigne = ligne.split(";", 3);
+                String[] elementLigne = ligne.split(";", 3); // SEGMENTATION LIGNE PAR 3 GRACE  AU DÉLIMITEUR ";" ET STOCKAGE DANS UN TABLEAU
   
-               if (chaine.equals(elementLigne[1]))
+               if (chaine.equals(elementLigne[1])) // SI LA CHAINE = L'ÉLÉMENT DU TABLEAU  SA VEUT DIRE LA REQUETE A DEJA ETAIS RÉALISER ET LE FICHIER CONTIENT DEJA LES INFOS 
                {
                     System.out.println("calcul effectuer le "+ elementLigne[0] + " retournant le resultat de " + elementLigne[2] +" characteres unique dans la chaine ");
-                    lectureFichier.close();
-                    return true ; 
+                    lectureFichier.close(); // ON FERME LE FICHIER 
+                    return true ;  // ON RETOURNE TRUE CAR LE FICHIER CONTENNER BIEN LES INFOS 
                }
             }
             lectureFichier.close();
@@ -46,29 +48,51 @@ class Traitement
 	        System.out.println("Erreur d'ouverture fichier ");
         }
         
-        return false ; 
+        return false ;  // ON RETOURE FALSE SI LE FICHIER NE CONTIENT PAS LES INFOS 
         
     }
 
-    public static  int calcul(String chaine )
+    public static  int calcul(String chaine ) // METHODE QUI PREND EN PARAMETTRE UNE CHAINE ET RETOURNE LE NOMBRE DE CHARACTERE UNIQUE
     {
-        ArrayList<Character> liste = new ArrayList<>();
+        ArrayList<Character> liste = new ArrayList<>();  // CRÉATION D'UNE "liste" DE TYPE ARRAYLIST QUI VAS PRENDRE CHAQUE CHARACTERES DE LA CHAINE
 
-        for (int i =0 ; i <chaine.length(); i++)
-            liste.add(chaine.charAt(i));
-            int nombre = 0 ;
-            for  (Character el1 : liste)
+        for (int i =0 ; i <chaine.length(); i++) // PARCOUR DE LA CHAINE 
+            liste.add(chaine.charAt(i)); // AJOUT DES CHARACTERES DE LA CHAINE A LA LISTE
+            int nombre = 0 ;   // COMPTEUR
+            for  (Character el1 : liste) // PARCOUR LA LISTE 
             {   
-                int i = 0 ;
-                for ( Character el2 : liste )
+                int i = 0 ;    // COMPTEUR 
+                for ( Character el2 : liste ) // PARCOUR LA LISTE 
                 { 
-                    if (el2.equals(el1))
-                        i++ ; 
+                    if (el2.equals(el1))  // SI ÉLÉMENT DE LA 1ER BOUCLE VAUT ÉLÉMENT DE LA 2EME 
+                        i++ ;             // INCREMENTE COMPTEUR "i"
                 }
-               if (i == 1 )
-                    nombre ++ ; 
+               if (i == 1 )             // SI "i" VAUT 1 
+                    nombre ++ ;     //INCREMENTE  COMPTEUR "nombre"
             }
-            return nombre;
+            return nombre;       // RETOURNE  "nombre "
     }  
-      
+      public static void cache()  // METHODE QUI CRÉE LE FICHIER CACHE  OU LE SUPPRIME SI SON POID ET TROP IMPORTANT 
+      {
+        File fichier =new File(nomFichier); //APPELLE FICHIER 
+        if(fichier.exists()) // VÉRIFICATION SI IL EXISTE 
+        {
+            double bytes = fichier.length(); // RÉCUPÉRATION DU POIDS EN OCTETS
+            double kilobytes = bytes / 1000; // CALCUL DU POIDS EN KILO OCTETS
+            if (kilobytes >= 100)  // SI LE POIDS EST SUPPÉRIEUR A 100 KO 
+                fichier.delete();  // ON DÉTRUIT LE FICHIER       
+        }
+        else  // SI LE FICHIER EXISTE PAS 
+        {   
+            try 
+            {
+                fichier.createNewFile(); // ON LE CRÉE 
+            }
+            catch(Exception e )  // CAPTURE DES EXCEPTION LIER A LA CRÉATION DU FICHIER 
+            {
+                System.out.println("impossible de crée le fichier ");
+            }
+        }   
+       
+      }
 }
